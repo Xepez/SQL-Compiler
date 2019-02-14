61995783ed2a8619bce81dad89223275144c6ccc
Xepez <Xepez9@gmail.com>
2019-02-13 22:19:04 -0800

    added most fcns for catalog

--- Catalog.cc
+++ Catalog.cc
@@ -35,29 +35,24 @@ bool Catalog::Save() {
 bool Catalog::GetNoTuples(string& _table, unsigned int& _noTuples) {
 
     sqlite3_stmt *stmt;
-
-    cout << "getting with " << _table << " w/ noTup: " << _noTuples << endl;
     
     // Prepares Select for tablename and numTuples
-    int rc = sqlite3_prepare_v2(db, "SELECT numTuples FROM table WHERE tablename = ?", -1, &stmt, NULL);
+    int rc = sqlite3_prepare_v2(db, "SELECT numTuples FROM table_info WHERE tablename = ?", -1, &stmt, NULL);
     
     if (rc == SQLITE_OK) {
         // Binds the table we are looking for
         sqlite3_bind_text(stmt, 1, _table.c_str(), -1, NULL);
     } else {
-        cout << "Failed to execute statement" << endl;
+        cout << "Failed to execute statement: " << sqlite3_errmsg(db) << endl;
     }
     
     int step = sqlite3_step(stmt);
     
 	if(step == SQLITE_ROW){
-        cout << "returned true " << endl;
         _noTuples = sqlite3_column_int(stmt, 0);
-        cout << "New NoTuple; " << _noTuples << endl;
         sqlite3_finalize(stmt);
 		return true;
 	} else {
-        cout << "returned false" << endl;
         sqlite3_finalize(stmt);
         return false;
 	}
@@ -67,10 +62,8 @@ void Catalog::SetNoTuples(string& _table, unsigned int& _noTuples) {
     
     sqlite3_stmt *stmt;
     
-    cout << "setting with " << _table << " w/ noTup: " << _noTuples << endl;
-    
     // Prepares Insert for numTuples in table
-    sqlite3_prepare_v2(db, "INSERT INTO table (numTuples) VALUES (?) WHERE tablename = ?", -1, &stmt, NULL);
+    sqlite3_prepare_v2(db, "UPDATE table_info SET numTuples = ? WHERE tablename = ?", -1, &stmt, NULL);
     // Binds the value and table we are looking for
     sqlite3_bind_int(stmt, 1, _noTuples);
     sqlite3_bind_text(stmt, 2, _table.c_str(), -1, NULL);
@@ -78,33 +71,110 @@ void Catalog::SetNoTuples(string& _table, unsigned int& _noTuples) {
     int rc = sqlite3_step(stmt);
 
     if (rc != SQLITE_DONE) {
-        cout << "ERROR inserting data" << endl;
+        cout << "ERROR inserting data -> " << sqlite3_errmsg(db) << endl;
     }
     
     sqlite3_finalize(stmt);
 }
 
 bool Catalog::GetDataFile(string& _table, string& _path) {
-	return true;
+	
+    sqlite3_stmt *stmt;
+    
+    // Prepares Select for tablename and numTuples
+    int rc = sqlite3_prepare_v2(db, "SELECT path FROM table_info WHERE tablename = ?", -1, &stmt, NULL);
+    
+    if (rc == SQLITE_OK) {
+        // Binds the table we are looking for
+        sqlite3_bind_text(stmt, 1, _table.c_str(), -1, NULL);
+    } else {
+        cout << "Failed to execute statement: " << sqlite3_errmsg(db) << endl;
+    }
+    
+    int step = sqlite3_step(stmt);
+    
+    if(step == SQLITE_ROW){
+        _path = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
+        sqlite3_finalize(stmt);
+        return true;
+    } else {
+        sqlite3_finalize(stmt);
+        return false;
+    }
 }
 
 void Catalog::SetDataFile(string& _table, string& _path) {
+    
+    sqlite3_stmt *stmt;
+    
+    // Prepares Insert for numTuples in table
+    sqlite3_prepare_v2(db, "UPDATE table_info SET path = ? WHERE tablename = ?", -1, &stmt, NULL);
+    // Binds the value and table we are looking for
+    sqlite3_bind_text(stmt, 1, _path.c_str(), -1, NULL);
+    sqlite3_bind_text(stmt, 2, _table.c_str(), -1, NULL);
+    
+    int rc = sqlite3_step(stmt);
+    
+    if (rc != SQLITE_DONE) {
+        cout << "ERROR inserting data -> " << sqlite3_errmsg(db) << endl;
+    }
+    
+    sqlite3_finalize(stmt);
 }
 
 bool Catalog::GetNoDistinct(string& _table, string& _attribute, unsigned int& _noDistinct) {
-	return true;
+
+    sqlite3_stmt *stmt;
+    
+    // Prepares Select for numDistinct
+    int rc = sqlite3_prepare_v2(db, "SELECT numDistinct FROM table_info, attribute WHERE tablename = ? AND attribute.tableid = table_info.tableid AND attributename = ?", -1, &stmt, NULL);
+    
+    if (rc == SQLITE_OK) {
+        // Binds the table we are looking for
+        sqlite3_bind_text(stmt, 1, _table.c_str(), -1, NULL);
+        sqlite3_bind_text(stmt, 2, _attribute.c_str(), -1, NULL);
+    } else {
+        cout << "Failed to execute statement: " << sqlite3_errmsg(db) << endl;
+    }
+    
+    int step = sqlite3_step(stmt);
+    
+    if(step == SQLITE_ROW){
+        _noDistinct = sqlite3_column_int(stmt, 0);
+        sqlite3_finalize(stmt);
+        return true;
+    } else {
+        sqlite3_finalize(stmt);
+        return false;
+    }
 }
 void Catalog::SetNoDistinct(string& _table, string& _attribute, unsigned int& _noDistinct) {
+    
+    sqlite3_stmt *stmt;
+    
+    // Prepares Insert for numDistinct in table
+    sqlite3_prepare_v2(db, "UPDATE attribute SET numDistinct = ? WHERE attribute.tableid = (SELECT tableid FROM table_info WHERE tablename = ?) AND attributename = ?;", -1, &stmt, NULL);
+    // Binds the value and table we are looking for
+    sqlite3_bind_int(stmt, 1, _noDistinct);
+    sqlite3_bind_text(stmt, 2, _table.c_str(), -1, NULL);
+    sqlite3_bind_text(stmt, 3, _attribute.c_str(), -1, NULL);
+    
+    int rc = sqlite3_step(stmt);
+    
+    if (rc != SQLITE_DONE) {
+        cout << "ERROR inserting data -> " << sqlite3_errmsg(db) << endl;
+    }
+    
+    sqlite3_finalize(stmt);
 }
 
 void Catalog::GetTables(vector<string>& _tables) {
     
     sqlite3_stmt *stmt;
+    int step;
     
     // Prepares Select for tablename
-    sqlite3_prepare_v2(db, "SELECT tablename FROM table", -1, &stmt, NULL);
-    
-    int step = sqlite3_step(stmt);
+    sqlite3_prepare_v2(db, "SELECT tablename FROM table_info", -1, &stmt, NULL);
     
     while ((step = sqlite3_step(stmt)) == SQLITE_ROW){
         // Convert to a string
@@ -117,10 +187,26 @@ void Catalog::GetTables(vector<string>& _tables) {
 }
 
 bool Catalog::GetAttributes(string& _table, vector<string>& _attributes) {
+    
+    sqlite3_stmt *stmt;
+    int step;
+    
+    // Prepares Select for attributename
+    sqlite3_prepare_v2(db, "SELECT attributename FROM attribute", -1, &stmt, NULL);
+    
+    while ((step = sqlite3_step(stmt)) == SQLITE_ROW){
+        // Convert to a string
+        string temp = string(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)));
+        // Gets each attribute name
+        _attributes.push_back(temp);
+    }
+    
+    sqlite3_finalize(stmt);
 	return true;
 }
 
 bool Catalog::GetSchema(string& _table, Schema& _schema) {
+    // Will work on tomorrow
 	return true;
 }
 
@@ -208,5 +294,6 @@ bool Catalog::DropTable(string& _table) {
 }
 
 ostream& operator<<(ostream& _os, Catalog& _c) {
+    // Will work on tomorrow
 	return _os;
 }
--- main.cc
+++ main.cc
@@ -10,135 +10,131 @@ using namespace std;
 
 int main () {
      
-	string table = "region", attribute, type;
-	vector<string> attributes, types;
-	vector<unsigned int> distincts;
-
-	attribute = "r_regionkey"; attributes.push_back(attribute);
-	type = "INTEGER"; types.push_back(type);
-	distincts.push_back(5);
-	attribute = "r_name"; attributes.push_back(attribute);
-	type = "STRING"; types.push_back(type);
-	distincts.push_back(5);
-	attribute = "r_comment"; attributes.push_back(attribute);
-	type = "STRING"; types.push_back(type);
-	distincts.push_back(5);
-
-	Schema s(attributes, types, distincts);
-	Schema s1(s), s2; s2 = s1;
-
-	string a1 = "r_regionkey", b1 = "regionkey";
-	string a2 = "r_name", b2 = "name";
-	string a3 = "r_commen", b3 = "comment";
-
-	s1.RenameAtt(a1, b1);
-	s1.RenameAtt(a2, b2);
-	s1.RenameAtt(a3, b3);
-
-	s2.Append(s1);
-
-	vector<int> keep;
-	keep.push_back(5);
-	keep.push_back(0);
-	s2.Project(keep);
-
-	cout << s << endl;
-	cout << s1 << endl;
-	cout << s2 << endl;
-    
+//    string table = "region", attribute, type;
+//    vector<string> attributes, types;
+//    vector<unsigned int> distincts;
+//
+//    attribute = "r_regionkey"; attributes.push_back(attribute);
+//    type = "INTEGER"; types.push_back(type);
+//    distincts.push_back(5);
+//    attribute = "r_name"; attributes.push_back(attribute);
+//    type = "STRING"; types.push_back(type);
+//    distincts.push_back(5);
+//    attribute = "r_comment"; attributes.push_back(attribute);
+//    type = "STRING"; types.push_back(type);
+//    distincts.push_back(5);
+//
+//    Schema s(attributes, types, distincts);
+//    Schema s1(s), s2; s2 = s1;
+//
+//    string a1 = "r_regionkey", b1 = "regionkey";
+//    string a2 = "r_name", b2 = "name";
+//    string a3 = "r_commen", b3 = "comment";
+//
+//    s1.RenameAtt(a1, b1);
+//    s1.RenameAtt(a2, b2);
+//    s1.RenameAtt(a3, b3);
+//
+//    s2.Append(s1);
+//
+//    vector<int> keep;
+//    keep.push_back(5);
+//    keep.push_back(0);
+//    s2.Project(keep);
+//
+//    cout << s << endl;
+//    cout << s1 << endl;
+//    cout << s2 << endl;
+//
     cout << "-----------------------------------------------------" << endl;
     cout << "Project 1:" << endl;
     
 	string dbFile = "catalog.sqlite";
 	Catalog c(dbFile);
-
-    unsigned int testTup = 0;
-    string test = "test";
-    c.GetNoTuples(test, testTup);
-    cout << testTup << endl;
-    testTup = 69;
-    c.SetNoTuples(test, testTup);
-    testTup = 0;
-    c.GetNoTuples(test, testTup);
-    cout << testTup << endl;
     
-    vector<string> listOfTables;
-    c.GetTables(listOfTables);
-    for (vector<string>::iterator itT = listOfTables.begin(); itT != listOfTables.end(); itT++) {
-        cout << *itT;
+    // Testing Purposes May Remove Later
+    string table = "test";
+    string attribute = "test_key";
+    vector<string> attributes;
+    attributes.push_back(attribute);
+    attributes.push_back("test_profile");
+    attributes.push_back("test_cal");
+    // -----------------------------------
+    
+    // Creates a Table
+//    c.CreateTable(table, attributes, types);
+
+    // Display Current Catalog
+    cout << c << endl;
+
+    // Changes # of Tuples
+    unsigned int temp = 9;
+    c.SetNoTuples(table, temp);
+
+    // If Table Exists Display # of Tuples
+    unsigned int tupleCount = 0;
+    if (c.GetNoTuples(table, tupleCount))
+        cout << "# of Tuples: " << tupleCount << endl;
+
+    // Changes Data Path
+    string setPath = table + ".dat";
+    c.SetDataFile(table, setPath);
+
+    // If Table Exists Display File Path
+    string path;
+    if (c.GetDataFile(table, path))
+        cout << "Path: " << path << endl;
+
+    // Changes # of Distinct Elements
+    unsigned int setNoDist = 5;
+    for (vector<string>::iterator itD = attributes.begin(); itD != attributes.end(); itD++) {
+        c.SetNoDistinct(table, *itD, setNoDist);
+        setNoDist++;
+    }
+
+    // If Table Exists Display # of Distinct Elements
+    cout << "# of Distinct: ";
+    unsigned int noDistinct = 0;
+    for (vector<string>::iterator itD = attributes.begin(); itD != attributes.end(); itD++) {
+        if (c.GetNoDistinct(table, *itD, noDistinct))
+            cout << noDistinct << " from attribute " << *itD << ", ";
     }
     cout << endl;
-//    // Creates a Table
-//    c.CreateTable(table, attributes, types);
-//
-//    // Display Current Catalog
-//    cout << c << endl;
-//
-//    // Changes # of Tuples
-//    unsigned int temp = 9;
-//    c.SetNoTuples(table, temp);
-//
-//    // If Table Exists Display # of Tuples
-//    unsigned int tupleCount = 0;
-//    if (c.GetNoTuples(table, tupleCount))
-//        cout << "# of Tuples: " << tupleCount << endl;
-//
-//    // Changes Data Path
-//    string setPath = table + ".dat";
-//    c.SetDataFile(table, setPath);
-//
-//    // If Table Exists Display File Path
-//    string path;
-//    if (c.GetDataFile(table, path))
-//        cout << "Path: " << path << endl;
-//
-//    // Changes # of Distinct Elements
-//    unsigned int setNoDist = 5;
-//    for (vector<string>::iterator itD = attributes.begin(); itD != attributes.end(); itD++) {
-//        c.SetNoDistinct(table, *itD, setNoDist);
-//        setNoDist++;
-//    }
-//
-//
-//    // If Table Exists Display # of Distinct Elements
-//    unsigned int noDistinct = 0;
-//    for (vector<string>::iterator itD = attributes.begin(); itD != attributes.end(); itD++) {
-//        if (c.GetNoDistinct(table, *itD, noDistinct))
-//            cout << "# of Distinct: " << noDistinct << " from attribute " << *itD;
-//    }
-//
-//    // Displays all Tables in DB
-//    vector<string> listOfTables;
-//    c.GetTables(listOfTables);
-//    for (vector<string>::iterator itT = listOfTables.begin(); itT != listOfTables.end(); itT++) {
-//        cout << *itT;
-//    }
-//    cout << endl;
-//
-//    // Displays all Attributes in a Table
-//    vector<string> listOfAtts;
-//    c.GetAttributes(table, listOfAtts);
-//    for (vector<string>::iterator itA = listOfAtts.begin(); itA != listOfAtts.end(); itA+) {
-//        cout << *itA;
-//    }
-//    cout << endl;
-//
-//    // Gets the Schema of the table
-//    Schema schema;
-//    c.GetSchema(table, schema);
-//    cout << schema << endl;
-//
-//    // Print Table
-//    cout << c << endl;
-//
-//    // Saves altered contents
-//    c.Save();
-//
-//    // Drops the Table
-//    c.DropTable(table);
-//
-//    // Display Current Catalog
-//    cout << c << endl;
-//
-//    return 0;
+
+    // Displays all Tables in DB
+    vector<string> listOfTables;
+    c.GetTables(listOfTables);
+    cout << "List of Tables: ";
+    for (vector<string>::iterator itT = listOfTables.begin(); itT != listOfTables.end(); itT++) {
+        cout << *itT << ", ";
+    }
+    cout << endl;
+
+    // Displays all Attributes in a Table
+    vector<string> listOfAtts;
+    c.GetAttributes(table, listOfAtts);
+    cout << "List of Attributes: ";
+    for (vector<string>::iterator itA = listOfAtts.begin(); itA != listOfAtts.end(); itA++) {
+        cout << *itA << ", ";
+    }
+    cout << endl;
+
+    // Gets the Schema of the table
+    Schema schema;
+    c.GetSchema(table, schema);
+    cout << "Schema for " << table << " : " << schema << endl;
+
+    // Print Table
+    cout << c << endl;
+
+    // Saves altered contents
+    c.Save();
+
+    // Drops the Table
+    c.DropTable(table);
+
+    // Display Current Catalog
+    cout << c << endl;
+
+    return 0;
 }
